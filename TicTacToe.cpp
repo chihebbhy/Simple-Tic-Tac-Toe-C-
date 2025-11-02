6de725b95c733ef137400fb2f01ffd45a9bb994c
chihebbhy <chiheb147bhy@gmail.com>
2025-11-02 20:48:38 +0100

    Add files via upload

---
+++ TicTacToe.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+
+using namespace std;
+
+class Game
+{
+public:
+
+    char Board[3][3];           // T holder , X for x , O for o
+    bool GameOver = false;
+    int TurnsPlayed = 0;
+    string Winner;
+    int Turn = 1;               // Turn == 1 : X plays ; else -1 plays ;
+
+
+    void InitBoard()            // Initialise Board with T
+    {
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                Board[i][j] = '0' + 3*i + j + 1;
+            }
+        }
+    }
+    Game()                      // Constructor
+    {
+        InitBoard();
+    }
+
+    void Init()
+    {
+        InitBoard();
+        GameOver = false;
+        TurnsPlayed = 0;
+        Turn = 1;
+    }
+    void PrintGame()
+    {
+        for(int i = 0 ; i<3 ; i++)
+        {
+            cout << ' ' << Board[i][0]  << " | " << Board[i][1] << " | " << Board[i][2] << endl;
+            if(i!=2)
+            {
+                cout << "---+---+---"<< endl;
+            }
+        }
+    }
+    bool PlayTurn()
+    {
+        char Player = (Turn == 1) ? 'X' : 'O';
+        int Choice ;
+
+        cout << Player << "'s turn to play." << endl;
+        cout << "Choose Case number: ";
+        cin >> Choice;
+        while( Choice < 1 || Choice > 9 || Board[(Choice-1) / 3][(Choice-1)%3 ] != ('0'+Choice) )
+        {
+            cout << "This case is either already filled or out of bounds [1-9] " << endl;
+            cout << "Choose Case number: ";
+            cin >> Choice;
+        }
+        Board[(Choice-1) / 3][(Choice-1)%3 ] = Player;
+        Turn *= -1; // X = 1 -> -1 = O      O = -1 -> 1 = X
+        TurnsPlayed ++;
+        CheckWin();
+        return true;
+    }
+    void CheckWin()
+    {
+        bool ok = false;
+        for(int i=0; i<3; i++)
+        {
+            ok |= Board[i][0] == Board[i][1] && Board[i][1]==Board[i][2];
+        }
+        for(int j=0; j<3; j++)
+        {
+            ok |= Board[0][j] == Board[1][j] && Board[1][j]==Board[2][j];
+        }
+        ok |= Board[0][0] == Board[1][1] && Board[1][1] ==Board[2][2];
+        ok |= Board[0][2] == Board[1][1] && Board[1][1] ==Board[2][0];
+        if(ok)
+        {
+            GameOver = true;
+            Winner = (Turn == 1) ? "O" : "X";
+        }
+        else if(TurnsPlayed >=9)
+        {
+            GameOver = true;
+            Winner = "Draw";
+        }
+        else
+        {
+            GameOver = false;
+        }
+
+    }
+    void Play()
+    {
+        //system("clear");
+        while(!GameOver)
+        {
+            PrintGame();
+            PlayTurn();
+            system("cls");
+        }
+        cout << "GAME OVER CONGRATS " << Winner ;
+        if(Winner != "Draw"){
+            cout << " won fair and square ";
+        }
+        cout << endl;
+        cout << "ENTER 1 TO PLAY AGAIN : ";
+        int again;
+        cin >> again ;
+        if(again == 1)
+        {
+            Init();
+            Play();
+        }
+    }
+
+};
+
+int main()
+{
+    Game g;
+    g.Play();
+    return 0;
+}
